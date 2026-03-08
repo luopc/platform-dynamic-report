e2a2a0f0438739e477fef35e7faa137c1ed1cf99
Robin Luo <robin_luopc@163.com>
2026-03-08 23:47:45 +0800

    [2026.03.08] service init.

---
+++ dynamic-report-server/src/main/java/com/luopc/platform/web/DynamicReportServiceApplication.java
@@ -0,0 +1,15 @@
+package com.luopc.platform.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@SpringBootApplication
+@EnableScheduling
+public class DynamicReportServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicReportServiceApplication.class, args);
+    }
+}
